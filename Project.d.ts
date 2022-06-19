fa88b9f8fab437cf4abca4151b2f946082878b5f
biodiscus <biodiscus@gmail.com>
2022-06-19 15:01:51 +0200

    fix: correct config of Vue config

--- Project.d.ts
+++ Project.d.ts
@@ -5,12 +5,17 @@ declare module "*.vue" {
 }
 
 declare module "*.pug" {
-  const template: string;
+  // NOTE for importing a Pug template in TS:
+  // - if is used the `method: 'compile'`, then template type is `Function` with the argument as an object {},
+  //   this argument should contains variables, e.g. {var_name: 'var_value'}, used in template function.
+  // - if is used the `method: 'render'`, then template type is `striing`,
+  //   but works with this "fake declaration" as `Function` too
+
+  const template: Function; // <-- HERE was false `string` type
   export default template;
 }
 
 declare module "pug-plugin" {
-
   import Webpack from "webpack";
 
   declare const PugPlugin: Webpack.Plugin;
