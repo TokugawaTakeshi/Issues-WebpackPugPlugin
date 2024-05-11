cd395160a14190881cdd31f5527d9f6c7903b89f
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2024-05-11 11:48:38 +0900

    * Remove outdated dependencies * Update remain dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -28,27 +28,6 @@ const webpackConfig: Webpack.Configuration = {
         loader: "vue-loader"
       },
 
-      /* ==== hug-plain-loader/pug-html-loader ====================================================================== */
-      // {
-      //   test: /\.pug$/u,
-      //   oneOf: [
-      //     {
-      //       resourceQuery: /^\?vue/u,
-      //       use: [ "pug-plain-loader" ]
-      //     },
-      //     {
-      //       use: [
-      //         {
-      //           loader: "html-loader",
-      //           options: {
-      //             minimize: { caseSensitive: true }
-      //           }
-      //         },
-      //         "pug-html-loader"
-      //       ]
-      //     }
-      //   ]
-      // },
       /* ==== Pug plugin ============================================================================================ */
       {
         test: /\.vue.pug$/,
@@ -58,7 +37,7 @@ const webpackConfig: Webpack.Configuration = {
             resourceQuery: /.vue$/u,
             loader: '@webdiscus/pug-loader', // <-- it is same pug-loader as in PugPlugin.loader
             options: {
-              method: 'html', // render Pug into pure HTML string
+              mode: 'html', // render Pug into pure HTML string
             },
           },
           // allow import of Pug in JS/TS and for "other cases", if a file hasn't the extension `.vue`
@@ -66,8 +45,8 @@ const webpackConfig: Webpack.Configuration = {
             resourceQuery: ".vue.ts",
             loader: '@webdiscus/pug-loader', // <-- it is same pug-loader as in PugPlugin.loader
             options: {
-              method: 'compile', // compile Pug into template function, use it if you want pass custom data into template on clinet-side rendering.
-              //method: 'render', // compile Pug into template string, use if your template has't external custom data, generates smallest file
+              mode: 'compile', // compile Pug into template function, use it if you want pass custom data into template on clinet-side rendering.
+              //mode: 'render', // compile Pug into template string, use if your template has't external custom data, generates smallest file
               //esModule: true, // should be `true` for importing in ES module, e.g. `import tmpl from './myTemplate.pug';`
                               // defaults is false, used for CommonJS, e.g.  `const tmpl = require('./myTemplate.pug');`
                               // NOTE: this option is optional, if it is not defined, importing in ES module works too.
@@ -80,7 +59,7 @@ const webpackConfig: Webpack.Configuration = {
         test: /(?!.*\.vue\.pug$).*\.pug$/,
         loader: '@webdiscus/pug-loader',
         options: {
-          method: 'render'
+          mode: 'render'
         },
       },
 
